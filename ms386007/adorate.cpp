5099089c718b142decd70738325a434ea556ee00
Marek Smolarczyk <mareksmoalrczyk33@gmail.com>
2017-12-15 16:18:13 +0100

    Updated executable usage

--- ms386007/adorate.cpp
+++ ms386007/adorate.cpp
@@ -8,13 +8,16 @@
     const bool debug = false;
 #endif
 
+const long THREAD_MAX_N = 100;
+
 int main(int argc, char** argv) {
-    if (argc != 4) {
+    if (argc != 4) { // invalid number of parameters
         std::cerr << "usage: " << argv[0] <<
             " thread-count inputfile b-limit\n"
             " thread-count\t\tnumber of created threads\n"
             " inputile\t\tpath to file containing the graph's description\n"
-            " b-limit\t\tmaximum number of edges sharing a single vertex\n";
+            " b-limit\t\tpath to function returning maximum number of edges"
+            " incident with a given vertex\n";
         
         return 1;
     }
