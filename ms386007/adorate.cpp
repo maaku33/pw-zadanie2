12cf788a495c962abcabf64b58909fa7924fea0f
Marek Smolarczyk <mareksmoalrczyk33@gmail.com>
2017-12-15 19:59:55 +0100

    Implemented graph reading

--- ms386007/adorate.cpp
+++ ms386007/adorate.cpp
@@ -1,5 +1,10 @@
+#include "blimit.hpp"
 #include <iostream>
+#include <fstream>
+#include <limits>
 #include <string>
+#include <vector>
+#include <map>
 #include <thread>
 
 #ifdef DEBUG
@@ -8,7 +13,66 @@
     const bool debug = false;
 #endif
 
-const long THREAD_MAX_N = 100;
+using count_t = unsigned int;
+using node_t = unsigned long;
+using weight_t = unsigned long;
+using result_t = weight_t;
+
+const count_t THREAD_MAX_N = 100;
+
+class Graph {
+public:
+    using adjacency_list = std::vector<std::pair<weight_t, node_t>>;
+    using node_list = std::map<node_t, adjacency_list>;
+
+private:
+    node_list V;
+    
+public:
+    Graph() = default;
+
+    void addEdge(node_t v, node_t u, weight_t w);
+    node_list::const_iterator nodeItBegin() const { return V.begin(); }
+};
+
+void Graph::addEdge(node_t v, node_t u, weight_t w) {
+    // TODO: Merge same edges (leave heaviest) ?
+    V[v].push_back(std::make_pair(w, u));
+    V[u].push_back(std::make_pair(w, v));
+}
+
+bool createGraphFromFile(std::string &file, Graph &G) {
+    std::fstream fs;
+    fs.open(file, std::fstream::in);
+
+    if (fs.fail()) {
+        std::cerr << "Counld not open the file " << file << "\n";
+        return false;
+    }
+
+    int c;
+    while ((c = fs.peek()) == '#') {
+        fs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    node_t v, u;
+    weight_t w;
+    while (!fs.eof()) {
+        fs >> v >> u >> w;
+        if (fs.fail()) break;
+
+        G.addEdge(v, u, w);
+    }
+
+    fs.close();
+    return true;
+}
+
+result_t parrallelBSuitor(Graph &G,
+                          count_t (*b)(count_t, node_t),
+                          count_t method) {
+    return b(method, ((*(G.nodeItBegin())).second)[0].second);
+}
 
 int main(int argc, char** argv) {
     if (argc != 4) { // invalid number of parameters
@@ -21,8 +85,21 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    int threadCount = std::stoi(argv[1]), bLimit = std::stoi(argv[3]);
+    count_t threadCount = std::stoi(argv[1]), bLimit = std::stoi(argv[3]);
     std::string inputFilename(argv[2]);
 
+    if (threadCount > THREAD_MAX_N) {
+        std::cerr << "number of threads cannot exceed " << THREAD_MAX_N << "\n";
 
+        return 1;
+    }
+
+    Graph G = Graph();
+    if (!createGraphFromFile(inputFilename, G)) return 1;
+
+    for (count_t i = 0; i < bLimit; i++) {
+        std::cout << parrallelBSuitor(G, &bvalue, i) << "\n";
+    }
+
+    return 0;
 }
