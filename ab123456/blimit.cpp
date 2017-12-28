3f4e804907ebc04fb3fc2c2f0b9d4aac7402f630
Marek Smolarczyk <mareksmoalrczyk33@gmail.com>
2017-12-28 10:18:52 +0100

    removed sample program

--- ab123456/blimit.cpp
+++
@@ -1,9 +0,0 @@
-#include "blimit.hpp"
-
-unsigned int bvalue(unsigned int method, unsigned long node_id) {
-    switch (method) {
-    default: return node_id % 42;
-    case 0: return 4;
-    case 1: return 7;
-    }        
-}
--- ab123456/packme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#TODO: put your student id here
-STUDENTID=ab123456
-
-zip $STUDENTID.zip CMakeLists.txt adorate-42.cpp blimit.hpp blimit.cpp  
-mv $STUDENTID.zip ../sols/
