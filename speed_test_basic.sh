3df618a1a7a073eef3bfc60e84592d2c05be2eba
maaku33 <mareksmolarczyk33@gmail.com>
2018-04-11 19:13:16 +0200

    Deleted unnecessary files and renamed folders

--- speed_test_basic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..8}
-do
-    ./speed_test.sh -b 20 -t $i -s ./data-res/ ./ms386007-new/build/adorate ./data/road_PA.txt >> ./times/basic.txt
-    ./speed_test.sh -b 3 -t $i -s ./data-res/ ./ms386007-new/build/adorate ./data/kron_g500-logn21.mtx >> ./times/basic.txt
-done
